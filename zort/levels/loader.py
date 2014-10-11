9b8067e01523141a62a2fbaf21fe30bd08075cdb
Kevin Manire <kevin.manire@kaseya.com>
2014-10-10 21:41:39 -0700

    Fixed dynamic level loader

--- zort/levels/loader.py
+++ zort/levels/loader.py
@@ -2,12 +2,15 @@
 Loading code for the levels
 """
 
+import importlib
+
 import zort.levels
-from zort.resources import maps
+from zort import resources
 
 
 def load_level(level_name, level_scene):
+    level_module = importlib.import_module("zort.levels.%s" % level_name)
     level_module = getattr(zort.levels, level_name)
-    level_scene.view.data.load_from_file(maps[level_name])
+    level_scene.view.data.load_from_file(resources.maps[level_name])
     level_module.setup_level(level_scene)
     return level_module
